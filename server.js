54e787a74e4f51b29bf3607315246b1b0a7f580f
Maria Palacios <imarip09@gmail.com>
2018-08-08 14:50:24 -0500

    updated server file

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 8080);
+if (require.main === module) {
+    app.listen(process.env.PORT || 8080, () => {
+        console.info(`Your is listening on port ${process.env.PORT}`);
+    });
+}
 
 module.exports = app;
