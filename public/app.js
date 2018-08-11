efd536e12b2537341c76100c1b75c1ad8f5283de
Maria Palacios <imarip09@gmail.com>
2018-08-10 20:02:43 -0500

    added working functions for create, delete and check off tasks and updated html page

--- public/app.js
+++ public/app.js
@@ -5,31 +5,36 @@ const MOCK_TASK_DATA = {
             "id" : "0111111",
             "text" : "Go grocery shopping",
             "userId": "103555",
-            "date" : "08-11-18"
+            "date" : "08-11-18",
+            "checkedOff_date" : true
         },
         {
             "id" : "0222222",
             "text" : "Work out",
             "userId": "103555",
-            "date" : "08-11-18"
+            "date" : "08-11-18",
+            "checkedOff_date" : true
         },
         {
             "id" : "0333333",
             "text" : "Study and work on project",
             "userId": "103555",
-            "date" : "08-11-18"
+            "date" : "08-11-18",
+            "checkedOff_date" : true
         },
         {
             "id" : "0444444",
             "text" : "Cook dinner",
             "userId": "103555",
-            "date" : "08-11-18"
+            "date" : "08-11-18",
+            "checkedOff_date" : false
         },
         {
             "id" : "0555555",
             "text" : "Read at least two chapters of a book",
             "userId": "103555",
-            "date" : "08-11-18"
+            "date" : "08-11-18",
+            "checkedOff_date" : false
         }
     ]
 };
@@ -37,7 +42,7 @@ const MOCK_TASK_DATA = {
 // function to get tasks created. This function will change,
 // once the real API is created. Instead of setTimeout, make 
 // an AJAX call to actual API.
-function getCreatedTasks(callbackFn) {
+function getTasks(callbackFn) {
     setTimeout(function() {
         callbackFn(MOCK_TASK_DATA)
     }, 100);
@@ -46,15 +51,39 @@ function getCreatedTasks(callbackFn) {
 //function to display tasks. This will not change.
 function displayTasks(data) {
     for (index in data.tasks) {
-        $('.tasksListSection').append(
+        let checkedOffTask;
+        $('ul').append(
+            '<li>' + data.tasks[index].text + '</li>'
+        );
+            // if the task is checked off
+            if (data.tasks[index].checkedOff_date) {
+                $('li').css({'text-decoration': 'line-through'});
+            }
+    };
+    
+}
+
+function deleteTask(data) {
+    //let deletedTask = tasks.filter(task => task.id === "0111111");
+    //return deletedTask;
+    //console.log(deletedTask);
+
+    for (index in data.tasks) {
+        if (data.tasks[index].id !== "0111111") {
+        $('.deletedTask').append(
             '<p>' + data.tasks[index].text + '</p>'
         );
+    }
     };
+
 }
 
 function getAndDisplayTasks() {
-    getCreatedTasks(displayTasks);
+    getTasks(displayTasks);
+    getTasks(deleteTask);
+
 }
 
 $(getAndDisplayTasks);
+//$(deleteTask);
 
