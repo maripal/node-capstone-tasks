9f2720df684bb6d531ac38a51c94c81666a27bef
Maria Palacios <imarip09@gmail.com>
2018-08-16 16:50:15 -0500

    added delete post function

--- public/app.js
+++ public/app.js
@@ -1,37 +1,37 @@
 // create mock data for api calls, since I haven't built the actual API
-const MOCK_TASK_DATA = {
-    "tasks" : [
+const MOCK_POST_DATA = {
+    "posts" : [
         {
             "id" : "0111111",
-            "text" : "Go grocery shopping",
+            "text" : "Go skydiving",
             "userId": "103555",
             "date" : "08-11-18",
             "checkedOff_date" : true
         },
         {
             "id" : "0222222",
-            "text" : "Work out",
+            "text" : "Travel around Europe",
             "userId": "103555",
             "date" : "08-11-18",
             "checkedOff_date" : true
         },
         {
             "id" : "0333333",
-            "text" : "Study and work on project",
+            "text" : "Get a job as a software developer",
             "userId": "103555",
             "date" : "08-11-18",
             "checkedOff_date" : true
         },
         {
             "id" : "0444444",
-            "text" : "Cook dinner",
+            "text" : "Visit the Grand Canyon",
             "userId": "103555",
             "date" : "08-11-18",
             "checkedOff_date" : false
         },
         {
             "id" : "0555555",
-            "text" : "Read at least two chapters of a book",
+            "text" : "Read one book",
             "userId": "103555",
             "date" : "08-11-18",
             "checkedOff_date" : false
@@ -42,64 +42,67 @@ const MOCK_TASK_DATA = {
 // function to get tasks created. This function will change,
 // once the real API is created. Instead of setTimeout, make 
 // an AJAX call to actual API.
-function getTasks(callbackFn) {
+function getPosts(callbackFn) {
     setTimeout(function() {
-        callbackFn(MOCK_TASK_DATA)
+        callbackFn(MOCK_POST_DATA)
     }, 100);
 }
 
-//function to display tasks. This will not change.
-function displayTasks(data) {
-    for (index in data.tasks) {
-        $('.taskList').append(
-            '<li>' + data.tasks[index].text + '</li>'
+//function to display posts. This will not change.
+function displayPosts(data) {
+    for (index in data.posts) {
+        $('.postList').append(
+            '<li>' + data.posts[index].text + '</li>'
         );
-            // if the task is checked off
-            //if (data.tasks[index].checkedOff_date) {
+            // if the post is checked off
+            //if (data.posts[index].checkedOff_date) {
             //    $('li').css({'text-decoration': 'line-through'});
             //}
     };
     
 }
 
-function createATask() {
+function createAPost() {
     $('.create').on('click', function() {
         // open the pop up window to input a new task
-        $('.createNewTaskPopUp').toggle();
+        $('.createNewPostPopUp').toggle();
     });
 }
 
-// function to submit a new task to list
-function submitNewTaskButton() {
-    $('.js-create-new-task').submit(function(event) {
+// function to submit a new post to list
+function submitNewPostButton() {
+    $('.js-create-new-post').submit(function(event) {
         event.preventDefault();
-        let targetInput = $(event.currentTarget).find('#js-new-task-input');
-        let newTask = targetInput.val();
+        let targetInput = $(event.currentTarget).find('#js-new-post-input');
+        let newPost = targetInput.val();
         targetInput.val("");
-        console.log(newTask);
-        $('.taskList').append(` <li> ${newTask} </li>`);
-        // to hide new task input after submitting a new task
-        $('.createNewTaskPopUp').toggle();
+        console.log(newPost);
+        $('.postList').append(` <li> ${newPost} </li>`);
+        // to hide new post input after submitting a new post
+        $('.createNewPostPopUp').toggle();
     });
 }
 
-//function deleteTask(data) {
-//    for (index in data.tasks) {
-//        if (data.tasks[index].id !== "0111111") {
-//        $('.deletedTask').append(
-//            '<p>' + data.tasks[index].text + '</p>'
-//        );
-//    }
-//    };
-//}
-
-function getAndDisplayTasks() {
-    getTasks(displayTasks);
-    //getTasks(deleteTask);
+function deleteButton() {
+    $('.delete').on('click', function() {
+        $(this).toggleClass('deleting');
+        $('li').toggleClass('deleting');
+    });
 }
 
-$(getAndDisplayTasks);
-$(createATask);
-$(submitNewTaskButton);
-//$(deleteTask);
+function deletePost() {
+    $('ul').on('click', 'li', function() {
+        $(this).remove();
+    });
+}
+
+function getAndDisplayPosts() {
+    getPosts(displayPosts);
+}
+
+$(getAndDisplayPosts);
+$(createAPost);
+$(submitNewPostButton);
+$(deleteButton);
+$(deletePost);
 
