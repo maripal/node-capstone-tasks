7d429c964876d4a350f7c807053dc092be56a049
Maria Palacios <imarip09@gmail.com>
2018-08-21 16:50:11 -0500

    added posts-lists directory for model and router for posts

---
+++ posts-list/postslist-models.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const goalPostSchema = mongoose.Schema({
+    User : {type : mongoose.Schema.Types.ObjectId, ref:'User'},
+    Text : {type: String, required: true},
+    Created : {type: Date, default: Date.now},
+    //Completed: {type: boolean}
+});
+
+goalPostSchema.methods.serialize = function() {
+    return {
+        id: this._id,
+        user: this.user,
+        text: this.text,
+        created: this.created,
+        completed: this.completed
+    };
+};
+
+const GoalPost = mongoose.model('GoalPost', goalPostSchema);
+
+module.exports = {GoalPost};
