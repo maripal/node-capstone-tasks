adc3ba3254ef44db4830aeb65c85677d5346484e
Maria Palacios <imarip09@gmail.com>
2018-08-22 17:51:36 -0500

    fixed this file

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 "use strict";
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/node-postslist';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/node-postlist';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-node-postslist';
 exports.PORT = process.env.PORT || 8080;
