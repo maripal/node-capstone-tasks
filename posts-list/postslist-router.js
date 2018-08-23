009690a02b3aea01c59a2922ca4158aae71ea054
Maria Palacios <imarip09@gmail.com>
2018-08-23 18:56:40 -0500

    fixed post endpoint and added put and delete endpoints

--- posts-list/postslist-router.js
+++ posts-list/postslist-router.js
@@ -30,12 +30,70 @@ router.post('/', (req, res) => {
         console.error(message);
         return res.status(400).send(message);
     }
+    // this is for if user adds notes to post
+    let notes = [];
+    if (notes.length === 0) {
+        notes.push(req.body.notes);
+    }
+    // this is for if user adds images to post
+    let images = [];
+    if (images.length === 0) {
+        images.push(req.body.images);
+    }
     GoalPost
         .create({
-            text: req.body.title
+            text: req.body.text,
+            notes: req.body.notes,
+            images: req.body.images,
+            completed: false
         })
         .then(post => {
             return res.status(201).json(post.serialize());
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({error: 'Oops. Something went wrong.'});
+        });
+})
+
+// PUT endpoint
+router.put('/:id', (req, res) => {
+    const requiredField = 'text';
+    if(!(requiredField in req.body)) {
+        const message = `Missing require \`${requiredField}\` in request body`;
+        console.error(message);
+        return res.status(400).send(message);
+    }
+    if (req.params.id !== req.body.id) {
+        const message = `Request path id (${
+          req.params.id
+        }) and request body id ``(${req.body.id}) must match`;
+        console.error(message);
+        return res.status(400).send(message);
+      }
+    GoalPost
+        .update({
+            id: req.params.id,
+            text: req.body.text,
+            notes: req.body.notes,
+            images: req.body.images,
+            completed: false
+        })
+        .then(post => {
+            return res.status(204).end();
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({error: 'Oops. Something went wrong.'});
+        });
+})
+
+// DELETE endpoint
+router.delete('/:id', (req, res) => {
+    GoalPost
+        .findByIdAndRemove(req.params.id)
+        .then(post => {
+            return res.status(204).end();
         })
         .catch(err => {
             console.error(err);
