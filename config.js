f2b2d84e3e80e02538049eaf7c9aaf5da1f68caf
Maria Palacios <imarip09@gmail.com>
2018-08-23 18:54:27 -0500

    fixed database url value

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 "use strict";
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/node-postlist';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-node-postslist';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/posts-list-db';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-posts-list-db';
 exports.PORT = process.env.PORT || 8080;
