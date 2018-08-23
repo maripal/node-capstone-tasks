69d7c0ba60e63b2652ce995087219f4fe37725e2
Maria Palacios <imarip09@gmail.com>
2018-08-22 19:44:56 -0500

    updated GET endpoint

--- posts-list/postslist-router.js
+++ posts-list/postslist-router.js
@@ -4,11 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const router = express.Router();
 
-const {GoalPost} = require('./postslist-models');
+const { GoalPost } = require('./postslist-models');
 
 // GET endpoint
-/*router.get('/', (req, res) => {
-    GoalPost.get()
+router.get('/', (req, res) => {
+    GoalPost.find()
     .then(posts => {
         res.json(posts);
     })
@@ -16,15 +16,31 @@ const {GoalPost} = require('./postslist-models');
         console.error(err);
         res.status(500).json({error: 'Oops. Something went wrong.'});
     });
-})*/
-
-router.get('/', (req, res) => {
-    res.status(200).json({message: "Connected!"});
 })
 
-// POST endpoint
-/*router.post('/', (req, res) => {
-
+/*router.get('/', (req, res) => {
+    res.status(200).json({message: "Connected!"});
 })*/
+
+// POST endpoint
+router.post('/', (req, res) => {
+    const requiredField = 'text';
+    if (!(requiredField in req.body)) {
+        const message = `Missing required \`${requiredField}\` in request body`;
+        console.error(message);
+        return res.status(400).send(message);
+    }
+    GoalPost
+        .create({
+            text: req.body.title
+        })
+        .then(post => {
+            return res.status(201).json(post.serialize());
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({error: 'Oops. Something went wrong.'});
+        });
+})
 
 module.exports = router;
