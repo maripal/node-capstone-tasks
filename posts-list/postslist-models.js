32b448231ff346658722e3fad623c3da2dcf1a4c
Maria Palacios <imarip09@gmail.com>
2018-08-23 20:46:41 -0500

    added version key property to hide __v

--- posts-list/postslist-models.js
+++ posts-list/postslist-models.js
@@ -5,13 +5,15 @@ mongoose.Promise = global.Promise;
 
 
 const goalPostSchema = mongoose.Schema({
-    user : {type : mongoose.Schema.Types.ObjectId, ref:'User'},
+    //user : {type : mongoose.Schema.Types.ObjectId, ref:'User'},
     text : {type: String, required: true},
     notes : {type: String},
     images : {type: String},
     created : {type: Date, default: Date.now},
     completed: Boolean
-});
+}, 
+    // to hide the '__v' property
+    {versionKey: false});
 
 goalPostSchema.methods.serialize = function() {
     return {
