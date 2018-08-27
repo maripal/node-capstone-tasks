2a12b1f3ee3c98b4cba5d6f8ef0159b21ca289c7
Maria Palacios <imarip09@gmail.com>
2018-08-27 10:23:38 -0500

    updated users schema

--- users/users-models.js
+++ users/users-models.js
@@ -1,22 +1,22 @@
 "use strict";
 
 const mongoose = require('mongoose');
+const bcrypt = require('bycryptjs');
+
 mongoose.Promise = global.Promise;
 
 const userSchema = mongoose.Schema({
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
+    firstName: {type: String, default: ''},
+    lastName: {type: String, default: ''},
     userName: {type: String, required: true, unique: true},
     password: {type: String, required: true}
 });
 
 userSchema.methods.serialize = function() {
     return {
-        id: this._id,
-        firstName: this.firstName,
-        lastName: this.lastName,
-        userName: this.userName,
-        //password: this.password
+        firstName: this.firstName || '',
+        lastName: this.lastName || '',
+        userName: this.userName || ''
     };
 }
 
