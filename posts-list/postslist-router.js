482ad1eb0a6edf87b289ad4870a5a3a9cfbe238d
Maria Palacios <imarip09@gmail.com>
2018-09-06 13:37:04 -0500

    added multer and still working on its functions

--- posts-list/postslist-router.js
+++ posts-list/postslist-router.js
@@ -3,8 +3,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = express.Router();
+const multer = require('multer');
 
 const { GoalPost } = require('./postslist-models');
+
+//set storage engine
+const storage = multer.diskStorage({
+    destination: '',
+    filename: function(req, file, callback) {
+        callback(null, `${file.fieldname}-${Date.now()}-${path.extname(file.originalname)}`);
+    }
+})
+
+//initialize image upload
+const upload = multer({storage});
 
 // GET endpoint
 router.get('/', (req, res) => {
