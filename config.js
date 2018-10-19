1c8948b9da4da00f3a55bb74dcdcb3afe410974b
Maria Palacios <imarip09@gmail.com>
2018-10-19 16:10:21 -0500

    updated file

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/posts-list-db';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-posts-list-db';
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET || 'hellotwo';
+exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d'; 
