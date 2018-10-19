2d4787ba15d4dc2a52f76c13dab11d49c1b0796c
Maria Palacios <imarip09@gmail.com>
2018-10-19 16:06:13 -0500

    updated files

--- auth/strategies.js
+++ auth/strategies.js
@@ -7,8 +7,6 @@ const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const { User } = require('../users/users-models');
 const { JWT_SECRET } = require('../config');
 
-const dotenv = require('dotenv').config();
-
 const localStrategy = new LocalStrategy((username, password, callback) => {
     let user;
     User.findOne({username: username})
