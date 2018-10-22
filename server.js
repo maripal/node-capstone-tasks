7f5544c895a0b6362fdcb4b7d20f3d6249242b43
Maria Palacios <imarip09@gmail.com>
2018-10-22 18:22:38 -0500

    updated file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 "use strict";
 
-require('dotenv').load();
+if (process.env.NODE_ENV !== 'production') {require('dotenv').config()}
+
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
