87b728a084acc15315b1ca9605b92d1c8af39583
Maria Palacios <imarip09@gmail.com>
2018-10-24 17:25:29 -0500

    updated file

--- test/test-auth.js
+++ test/test-auth.js
@@ -6,8 +6,8 @@ const jwt = require('jsonwebtoken');
 
 const {app, runServer, closeServer} = require('../server');
 const {User} = require('../users/users-models');
-const {JWT_SECRET, TEST_DATABASE_URL} = require('../config');
-const JWT_SECRET = process.env.JWT_SECRET || 'whatever';
+const {TEST_DATABASE_URL} = require('../config');
+const JWT_SECRET = process.env.JWT_SECRET || 'dtexas';
 
 const expect = chai.expect;
 
